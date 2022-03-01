745faa19aef0ac9d941e570d7681885ae5541f56
M-MSilva <marcosmatheusdepaivasilva@gmail.com>
2022-03-01 14:04:20 -0300

    update Deploy

--- Deploy/LoanPredictionWebapp.py
+++ Deploy/LoanPredictionWebapp.py
@@ -1,8 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  1 11:03:02 2022
+
+@author: marquinho
+"""
+
 
 import pickle 
 import pandas as pd
 import streamlit as st
 import preprocessing as pre
+#import statements as ifs
 
 
 loaded_model = pickle.load(open('trained_model.sav','rb'))
@@ -27,28 +35,36 @@ def code():
     #name of the page
     st.title('Loan Prediction Web App') 
     
-    Gender	= st.text_input('What is your gender, Male or Female?')
-    Married	= st.text_input('Are you married, Yes or No?')
-    Dependents	= st.text_input('How many children do you have,1,2 or 3+ ?')
-    Education	= st.text_input('Do you have a degree? Graduate or Not Graduate')
-    Self_Employed	= st.text_input('Do you work for yourself? Yes or No')
-    ApplicantIncome	= st.number_input('what is your salary?')
-    CoapplicantIncome	= st.number_input('What is your partners salary? If you dont have a partner, enter 0.')
-    LoanAmount	= st.number_input('What is the requested amount?')
-    Loan_Amount_Term	= st.number_input('how long do you plan to pay off the debt? in days.', min_value=0, max_value=10000, value=0, step=1)
-    Credit_History	= st.number_input('Does the individual have a good credit history? 1 for good or 0 for bad', min_value=0, max_value=3, value=0, step=1)
-    Property_Area	= st.text_input('Does the applicant live in an Urban, Semiurban or Rural area?')
+    form = st.form(key='user')
+    Gender = form.selectbox('What is your gender?',['Male','Female'])
+    Married = form.selectbox('Are you married',['Yes','No'])
+    Dependents = form.selectbox('How many children or dependents do you have?',['1','2','3+']) 
+    Education = form.selectbox('Do you have a degree?',['Graduate','Not Graduate'])
+    Self_Employed = form.selectbox('Do you work for yourself?',['Yes','No'])
+    Credit_History = form.selectbox('Does the individual have a good credit history? 1 for good or 0 for bad',[1,0])
+    Property_Area = form.selectbox('Does the applicant live ?',['Urban', 'Semiurban','Rural'])
+    submit_button = form.form_submit_button('Add')
     
-    #insert and treatment of the data
+    
+    with st.form(key='numbers'):        
+        ApplicantIncome	= st.number_input('what is your salary?')
+        CoapplicantIncome	= st.number_input('What is your partners salary? If you dont have a partner, enter 0.')
+        LoanAmount	= st.number_input('What is the requested amount?')
+        Loan_Amount_Term	= st.number_input('how long do you plan to pay off the debt? in days.', min_value=0, max_value=10000, value=0, step=1)
+        submit_button2 = st.form_submit_button('Add')
+    
     data = [[Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area]]
     columns = ['Gender','Married','Dependents','Education','Self_Employed','ApplicantIncome','CoapplicantIncome','LoanAmount','Loan_Amount_Term','Credit_History','Property_Area']
     
+    
+    #insert
     df = pd.DataFrame(data=data, columns=columns)
     
+    
+    #treatment of the data
     if(Property_Area):
         df_prepared = pre.runPreprocessing(df)
-
-
+    
 
 
     #code for prediction
@@ -59,7 +75,6 @@ def code():
         Condition = Loan_prediction(df_prepared)
     
     st.success(Condition)
-
 
 if __name__ == '__main__':
     code()				
